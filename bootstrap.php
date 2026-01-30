441f69178900193b701c43757c3eb2350e47fc94
pnytrainings123-pny <hsuntariq@gmail.com>
2026-01-30 13:09:35 +0500

    navbar

---
+++ bootstrap.php
@@ -0,0 +1,14 @@
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.13.1/font/bootstrap-icons.min.css">
+
+<script defer src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js"
+    integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous">
+</script>
+
+<link rel="shortcut icon"
+    href="https://play-lh.googleusercontent.com/3hSaY9S6hkL2z1XB4sf942EZjdGCSmg7i4PhVeVHXFYq_qzPmOgwmDM_WYpuDerjlAM=s256-rw"
+    type="image/x-icon">
+
+<title>Cheezious</title>
